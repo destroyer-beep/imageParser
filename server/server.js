e115349b88cb33d0402bf7f7958905413a18bea6
Vladimir <destroyer.hramov@gmail.com>
2022-06-17 00:33:07 +0300

    refactor UNIX (ubuntu)

--- server/server.js
+++ server/server.js
@@ -1,22 +1,34 @@
 import express from 'express';
 import ImageController from './controllers/ImageController.js';
-import keys from 'global-keys';
-import screenshot from 'screenshot-desktop';
-import fs from "fs";
+// import keys from 'global-keys';
+// import screenshot from 'screenshot-desktop';
+// import fs from "fs";
+import screenshot from 'desktop-screenshot';
 
 const app = express();
-const keyStream = new keys.KeyStream();
 
-keyStream.on('data', async state => {
-    if(state.length === 1 && state[0] === 44) {
-        let response = await screenshot({format: 'jpg'})
-            const filename = Date.now();
-            await fs.writeFileSync(`./image/${filename}.jpg`, Buffer.from(response));
-            await ImageController.imgParser(filename);
+const filename = Date.now();
+screenshot(`./image/${filename}.jpg`, async (error, complete) => {
+    if(error) {
+        console.log("Screenshot failed", error);
+    } else {
+        await ImageController.imgParser(filename);
     }
-})
+});
 
-app.listen(3500, () => {
+// const keyStream = new keys.KeyStream();
+
+// keyStream.on('data', async state => {
+//     console.log(state);
+//     if(state.length === 1 && state[0] === 44) {
+// let response = await screenshot({format: 'jpg'})
+// const filename = Date.now();
+// await fs.writeFileSync(`./image/${filename}.jpg`, Buffer.from(response));
+// await ImageController.imgParser(filename);
+//     }
+// })
+
+app.listen(4900, () => {
     console.log('server started');
 });
 
